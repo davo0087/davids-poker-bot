740de403ba542b2361a95053f80f26cd4df51629
davo0087 <davinatorbossapril@gmail.com>
2025-06-03 17:18:05 -0400

    Update app.py

--- app.py
+++ app.py
@@ -4,6 +4,8 @@ import streamlit as st
 from treys import Card, Deck, Evaluator
 import random
 
+st.write("Loaded!")
+
 suits = {'♥️': 'h', '♦️': 'd', '♣️': 'c', '♠️': 's'}
 ranks = ['2', '3', '4', '5', '6', '7', '8', '9', 'T', 'J', 'Q', 'K', 'A']
 
